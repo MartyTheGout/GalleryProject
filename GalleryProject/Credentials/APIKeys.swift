af7e640876ec71d12ab20a36c1980a76714c268c
MartyTheGout <chihyung0@naver.com>
2025-01-17 13:58:58 +0900

    Initial Commit

---
+++ GalleryProject/Credentials/APIKeys.swift
@@ -0,0 +1,8 @@
+//
+//  APIKeys.swift
+//  GalleryProject
+//
+//  Created by marty.academy on 1/17/25.
+//
+
+import Foundation
---
+++ GalleryProject/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GalleryProject
+//
+//  Created by marty.academy on 1/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
